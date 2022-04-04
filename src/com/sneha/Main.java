113365e23b7fbb356ccea5435414da6046e118bb
SnehaMane17 <snehamane171998@gmail.com>
2022-04-04 18:48:37 +0530

    printing value using methods

--- src/com/sneha/Main.java
+++ src/com/sneha/Main.java
@@ -2,37 +2,21 @@ package com.sneha;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
+    public int printNumber(int a){
+        return a;
+    }
     public static void main(String[] args) throws FileNotFoundException {
         FileOutputStream fos = new FileOutputStream("D:\\projects\\sid.csv");
         PrintWriter pw = new PrintWriter(fos);
-        pw.println("Value,Date,Time");
-        Scanner sc = new Scanner(System.in);
+        Main obj = new Main();
+        pw.println(obj.printNumber(10));
+        pw.close();
+        System.out.println("File created");
 
-        while (true) {
-            System.out.println("Enter Value");
-            int value = sc.nextInt();
-            System.out.println("This is Value: " + value );
-
-            System.out.println("Enter Date");
-            String date = sc.next();
-            System.out.println("This is Date: " + date );
-
-            System.out.println("Enter Time");
-            String time = sc.next();
-            System.out.println("This is Time: " + time );
-
-            if (value==0){
-                break;
-            }
-
-            pw.println(value + "," + date + "," + time);
-
-        }
-
-
-
+//        obj.printNumber(6);
     }
 }
